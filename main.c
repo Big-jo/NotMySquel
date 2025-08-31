fc99f959ef777eefed38568edbe17cb8cb747d77
joseph <furiousjoe16@gmail.com>
2025-08-31 23:55:58 +0100

    minor btree implmentation

--- main.c
+++ main.c
@@ -56,8 +56,6 @@ const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 const uint32_t PAGE_SIZE = 4096;
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 /*
  * Shared Node Header Layout
@@ -88,7 +86,7 @@ const uint32_t LEAF_NODE_VALUE_OFFSET =
     LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
 const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
 const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
-const uint32_t LEAD_NODE_MAX_CELL_SIZE =
+const uint32_t LEAF_NODE_MAX_CELLS =
     LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 typedef struct {
@@ -109,6 +107,15 @@ typedef struct {
   uint32_t cell_num;
   bool end_of_table;
 } Cursor;
+
+void print_constants() {
+  printf("ROW_SIZE: %d\n", ROW_SIZE);
+  printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+  printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+  printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
 
 void print_row(Row *row) {
   printf("(%d, %s, %s)\n", row->id, row->username, row->email);
@@ -135,7 +142,7 @@ void *leaf_node_cell(void *node, uint32_t cell_num) {
 }
 
 uint32_t *leaf_node_key(void *node, uint32_t cell_num) {
-  return leaf_node_key(node, cell_num);
+  return leaf_node_cell(node, cell_num);
 }
 
 void *leaf_node_value(void *node, uint32_t cell_num) {
@@ -173,6 +180,7 @@ Pager *pager_open(const char *filename) {
 
 void *get_page(Pager *pager, uint32_t page_num) {
   if (page_num > TABLE_MAX_PAGES) {
+    printf("%d\n", page_num);
     printf("Page out of bound\n");
     exit(EXIT_FAILURE);
   }
@@ -206,6 +214,40 @@ void *get_page(Pager *pager, uint32_t page_num) {
   return pager->pages[page_num];
 }
 
+void print_contants() {
+  printf("ROW SIZE: %d\n", ROW_SIZE);
+  printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+  printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+  printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+void print_leaf_node(void *node) {
+  uint32_t num_cells = *leaf_node_num_cells(node);
+  printf("leaf (size %d)\n", num_cells);
+  for (uint32_t i = 0; i < num_cells; i++) {
+    uint32_t key = *leaf_node_key(node, i);
+    printf("  - %d : %d\n", i, key);
+  }
+}
+
+void leaf_node_insert(Cursor *cursor, uint32_t key, Row *row) {
+  void *node = get_page(cursor->table->pager, cursor->page_num);
+
+  uint32_t num_cells = *leaf_node_num_cells(node);
+  if (num_cells >= LEAF_NODE_MAX_CELLS) {
+    for (uint32_t i = num_cells; i < cursor->cell_num; i--) {
+      memcpy(leaf_node_num_cells(node), leaf_node_num_cells(node),
+             LEAF_NODE_CELL_SIZE);
+    }
+  }
+
+  *(leaf_node_num_cells(node)) += 1;
+  *(leaf_node_key(node, cursor->cell_num)) = key;
+  serialize_row(row, leaf_node_value(node, cursor->cell_num));
+}
+
 Table *db_open(char *filename) {
   Pager *pager = pager_open(filename);
   uint32_t num_rows = pager->file_length / ROW_SIZE;
@@ -223,14 +265,14 @@ Table *db_open(char *filename) {
   return table;
 }
 
-void *row_slot(Table *table, uint32_t row_num) {
-  uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void *page = get_page(table->pager, page_num);
-
-  uint32_t row_offset = row_num % ROWS_PER_PAGE;
-  uint32_t byte_offset = row_offset * ROW_SIZE;
-  return page + byte_offset;
-}
+// void *row_slot(Table *table, uint32_t row_num) {
+//   uint32_t page_num = row_num / ROWS_PER_PAGE;
+//   void *page = get_page(table->pager, page_num);
+//
+//   uint32_t row_offset = row_num % ROWS_PER_PAGE;
+//   uint32_t byte_offset = row_offset * ROW_SIZE;
+//   return page + byte_offset;
+// }
 
 void pager_flush(Pager *pager, uint32_t page_num) {
   if (pager->pages[page_num] == NULL) {
@@ -304,6 +346,17 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table) {
   if (strcmp(input_buffer->buffer, ".exit") == 0) {
     db_close(table);
     exit(EXIT_SUCCESS);
+  } else if (strcmp(input_buffer->buffer, ".help") == 0) {
+    printf("None Yet!\n");
+    return META_COMMAND_SUCCESS;
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+    printf("Tree\n");
+    print_leaf_node(get_page(table->pager, 0));
+    return META_COMMAND_SUCCESS;
+  } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+    printf("Constants\n");
+    print_constants();
+    return META_COMMAND_SUCCESS;
   } else {
     return META_COMMAND_UNRECOGNISED;
   }
@@ -369,51 +422,15 @@ PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement) {
   return PREPARE_SUCCESS;
 }
 
-ExecuteResult execute_insert(Statement *statement, Table *table) {
-  if (table->num_rows > TABLE_MAX_ROWS) {
-    return EXECUTE_TABLE_FULL;
-  }
-
-  Row *row_to_insert = &(statement->row_to_insert);
-
-  serialize_row(row_to_insert, row_slot(table, table->num_rows));
-  table->num_rows += 1;
-
-  return EXECUTE_SUCCESS;
-}
-
-ExecuteResult execute_select(Statement *statement, Table *table) {
-  Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
-    deserialize_row(row_slot(table, i), &row);
-    print_row(&row);
-  }
-  return EXECUTE_SUCCESS;
-}
-
-ExecuteResult execute_statement(Statement *statement, Table *table) {
-  switch (statement->type) {
-  case (STATEMENT_INSERT):
-    return execute_insert(statement, table);
-  case (STATMENT_SELECT):
-    return execute_select(statement, table);
-  }
-}
-
 void print_start_screen() {
-  printf("               ___\n");
-  printf("              |___|\n");
-  printf("              (o o)\n");
-  printf("          oOO--(_)--OOo\n");
-  printf("              |||\n");
-  printf("              |||\n");
-  printf("              |||\n");
-  printf("              |||\n");
-  printf("              |||\n");
-  printf("             /|||\\\n");
-  printf("            /_|||_\\\n");
-  printf("             |___|\n\n");
-
+  printf("   ____   ____   _      \n");
+  printf("  / ___| / __ \\ | |     \n");
+  printf(" | (___ | |  | || |     \n");
+  printf("  \\___ \\| |  | || |     \n");
+  printf("  ____) | |__| || |____ \n");
+  printf(" |_____/ \\____/ |______|\n");
+  printf("             |          \n");
+  printf("             Q          \n");
   printf("       OurSQL-BIH REPL ENGINE\n");
   printf("       Respectfully... run your queries\n");
   printf("       Version 0.1.0-dev\n\n");
@@ -439,6 +456,7 @@ void read_input(InputBuffer *input_buffer) {
 
 Cursor *table_start(Table *table) {
   Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
 
   cursor->page_num = table->root_page_num;
   cursor->cell_num = 0;
@@ -450,7 +468,28 @@ Cursor *table_start(Table *table) {
   return cursor;
 }
 
+Cursor *table_end(Table *table) {
+  Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+
+  cursor->page_num = table->root_page_num;
+
+  void *root_node = get_page(table->pager, table->root_page_num);
+  uint32_t num_cells = *leaf_node_num_cells(root_node);
+  cursor->cell_num = num_cells;
+  cursor->end_of_table = true;
+
+  return cursor;
+}
+
 void *cursor_value(Cursor *cursor) {
+  uint32_t page_num = cursor->page_num;
+
+  void *page = get_page(cursor->table->pager, page_num);
+  return leaf_node_value(page, cursor->cell_num);
+}
+
+void cursor_advance(Cursor *cursor) {
   uint32_t page_num = cursor->page_num;
   void *node = get_page(cursor->table->pager, page_num);
 
@@ -459,6 +498,45 @@ void *cursor_value(Cursor *cursor) {
     cursor->end_of_table = true;
   }
 }
+
+ExecuteResult execute_insert(Statement *statement, Table *table) {
+  void *node = get_page(table->pager, table->root_page_num);
+  if (*(leaf_node_num_cells(node)) >= LEAF_NODE_MAX_CELLS) {
+    return EXECUTE_TABLE_FULL;
+  }
+
+  Row *row_to_insert = &(statement->row_to_insert);
+  Cursor *cursor = table_end(table);
+
+  leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
+
+  free(cursor);
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement *statement, Table *table) {
+  Cursor *cursor = table_start(table);
+
+  Row row;
+
+  while (!(cursor->end_of_table)) {
+    uint32_t *c_value = cursor_value(cursor);
+    deserialize_row(c_value, &row);
+    print_row(&row);
+    cursor_advance(cursor);
+  }
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement *statement, Table *table) {
+  switch (statement->type) {
+  case (STATEMENT_INSERT):
+    return execute_insert(statement, table);
+  case (STATMENT_SELECT):
+    return execute_select(statement, table);
+  }
+}
+
 // Basic REPL-CLI (Sometimes you've got to learn to run before you can walk --
 // Tony Stank)
 int main(int argc, char *argv[]) {
--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -91,7 +91,7 @@ describe 'database' do
   it 'keeps data after closing connection' do
     result1 = run_script([
       "insert 1 user1 person1@example.com",
-      ".exit"match_array
+      ".exit"
     ])
     expect (result1).to match_array([
       "f_yeah_db 🤞🏾> Executed.",
@@ -107,4 +107,45 @@ describe 'database' do
     "db > ",
   ])
   end
+
+  it 'it allows printing structure of root node btree' do
+    script = [3,1,2].map do |i|
+      "insert #{i} user#{i} person#{i}@example.com"
+  end
+    script << ".btree"
+    script << ".exit"
+    result = run_script(script)
+
+    expect(result).to match_array([
+      "f_yeah_db 🤞🏾> executed.",
+      "f_yeah_db 🤞🏾> executed.",
+      "f_yeah_db 🤞🏾> executed.",
+      "leaf (size 3)",
+      " -0 : 3",
+      " -1 : 1",
+      " -2 : 2",
+      "db > "
+    ])
+
+    it 'prints constants' do
+    script = [
+      ".constants",
+      ".exit",
+    ]
+    result = run_script(script)
+
+    expect(result).to match_array([
+     "db > Constants:",
+     "ROW_SIZE: 293",
+      "COMMON_NODE_HEADER_SIZE: 6",
+      "LEAF_NODE_HEADER_SIZE: 10",
+      "LEAF_NODE_CELL_SIZE: 297",
+      "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+      "LEAF_NODE_MAX_CELLS: 13",
+      "db > ",
+    ])
+  end
+ end
 end
+
+
